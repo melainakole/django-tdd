3995b6d8abf2f7b1a28c63b2eb4db0e2717268e6
Matias <mggutierrez@uc.cl>
2021-01-09 11:18:45 -0300

    Added new List object with a one-to-many relationship to Item

--- lists/models.py
+++ lists/models.py
@@ -4,8 +4,10 @@ from django.db.models.base import Model
 # Create your models here.
 
 
+class List(Model):
+    pass
+
+
 class Item(Model):
-    """
-    docstring
-    """
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
@@ -17,5 +17,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST.get('item_text'))
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST.get('item_text'), list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
