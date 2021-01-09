beb21d934dee3db2868adb2f922d1cd75758b459
Matias <mggutierrez@uc.cl>
2021-01-09 09:40:39 -0300

    Expanded functional test for multiple lists functionality

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,5 @@
-from unittest.case import expectedFailure
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest, response
-from django.template.loader import render_to_string
 
-from lists.views import home_page
 from lists.models import Item
 # Create your tests here.
 
